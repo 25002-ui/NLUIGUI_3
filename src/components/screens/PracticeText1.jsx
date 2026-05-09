a07ea50fa3ab53e4f668f3574972088242cc0734
Takakura Yuka <iw25002@dwc.doshisha.ac.jp>
2026-05-09 15:58:01 +0900

    Update PracticeText1.jsx

--- src/components/screens/PracticeText1.jsx
+++ src/components/screens/PracticeText1.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './PracticeText1.css'
-import ScreenHeader from '../ScreenHeader'
+import React from 'react';
+import ScreenHeader from '../ScreenHeader';
+import './PracticeText1.css';
 
-export default function PracticeText1({ navigateTo, goBack, goHome }) {
-  const [inputText, setInputText] = useState('')
-
+const PracticeText1 = ({ navigateTo, goBack, goHome }) => {
   const handleInputClick = () => {
-    navigateTo('practice_text_2')
-  }
+    navigateTo('practice_text_2');
+  };
 
   return (
     <div className="screen-container">
@@ -17,6 +15,11 @@ export default function PracticeText1({ navigateTo, goBack, goHome }) {
         <div className="screen-content">
           <img src="/material/practice_text.png" alt="Practice Text" className="content-image" />
 
+          {/* 修正：画像のすぐ下にテキストボックスを配置 */}
+          <button className="text-input-box" onClick={handleInputClick}>
+            <span className="input-placeholder">文章を入力</span>
+          </button>
+
           <div className="description">
             以下の文章をテキスト入力してください。
           </div>
@@ -24,12 +27,10 @@ export default function PracticeText1({ navigateTo, goBack, goHome }) {
           <div className="example-text">
             玉ねぎと中華麺を使った、ピリ辛料理を教えて。洗い物が少なくて、ネギは使わず、15分以内で作れる料理でできれば簡単にお願い。
           </div>
-
-          <button className="text-input-box" onClick={handleInputClick}>
-            <span className="input-placeholder">文章を入力</span>
-          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default PracticeText1;
